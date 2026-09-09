0e2277792569896a9a3faa1c46755ce74ce952a8
JoshuaMViudes <contadojosh2@gmail.com>
2026-09-09 16:36:13 -0300

    Adicionar tarefa

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -4,6 +4,7 @@ import 'package:sqflite/sqflite.dart';
 class DatabaseHelper {
   static Database? _db;
 
+  //Abre(ou cria, se não existe) o arquivo banco de dados
   static Future<Database> abrirBanco() async {
     final caminho = join(await getDatabasesPath(), 'tarefas.db');
 
@@ -22,9 +23,25 @@ class DatabaseHelper {
     );
   }
 
+  // Getter que devolve o banco de dados já aberto, ou abre se ainda não existe
   static Future<Database> get database async {
     _db ??= await abrirBanco();
 
     return _db!;
+  }
+
+  // READ: Buscar todas as tarefas salvas no banco
+  static Future<List<Map<String, dynamic>>> buscarTarefas() async {
+    final db = await DatabaseHelper.database;
+    return db.query('tarefas'); //SELECT * FROM tarefas
+  }
+
+  //CREATE: Inserir uma nova tarefa no banco de dados
+  static Future<void> inserirTarefa(String titulo) async {
+    final db = await DatabaseHelper.database;
+    await db.insert('tarefas', {
+      'titulo': titulo,
+      'situacao': 0,
+    });
   }
 }
